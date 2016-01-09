99ba1487adc21029e2e05050596ee11d5fe8f093
Sumanvick <onlyforsuman@gmail.com>
2016-01-10 00:24:26 +0530

    Update VariableIntialization.java

--- VariableIntialization.java
+++ VariableIntialization.java
@@ -9,7 +9,7 @@ public class VariableIntialization {
     public static void main(String[] args) {
         int age="Suman";	// This intialization is not possible here ()"Suman") is a string and int variable (age) accepts  numeric value.
 		
-		int age=true;	// This intialization also is not possible here (true) is a boolean and int variable (age) accepts  numeric value.
+	int age=true;	// This intialization also is not possible here (true) is a boolean and int variable (age) accepts  numeric value.
     }
     
 }
