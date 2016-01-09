36c905a67bdc741439237c9eb8e94045ff06dfdc
Sumanvick <onlyforsuman@gmail.com>
2016-01-10 00:22:13 +0530

    Create VariableDeclaration.java
    /*Assignment 2
    * Problem Statement :
    * Declare three integer variables (a,b,c) in a single declaration and simultaneously initialize b to 4.
    * Expected Output :
    * Not applicable.*/

---
+++ VariableDeclaration.java
@@ -0,0 +1,13 @@
+/*Assignment 2
+* Problem Statement :
+* Declare three integer variables (a,b,c) in a single declaration and simultaneously initialize b to 4.
+* Expected Output :
+* Not applicable.*/
+
+public class VariableDeclaration {
+    
+    public static void main(String[] args) {
+        int a,b=4,c;
+    }
+    
+}
