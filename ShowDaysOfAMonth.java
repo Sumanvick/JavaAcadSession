eb7760d85da9bf428a7120f44738c1bb5311a3a5
Suman Sarkar <onlyforsuman@gmail.com>
2016-01-15 23:04:56 +0530

    Create ShowDaysOfAMonth.java

---
+++ ShowDaysOfAMonth.java
@@ -0,0 +1,57 @@
+/*Session 2
+* Assignment 4
+* Problem Statement :
+* Write a program to print total number of days in the given month.
+* Expected Output :
+* Should print total number of days in the given month.	*/
+
+public class ShowDaysOfAMonth {
+    
+    public static void main(String[] args) {
+        String month;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Month: ");
+        month= sc.next();
+        month = month.toLowerCase();
+        switch(month){
+            case "january" : 
+            case "jan" :   System.out.print("The No Of Days In This Month : 31 Days");
+                           break;
+            case "february" : 
+            case "feb" :   System.out.print("The No Of Days In This Month : 28 days, 29 days in Leap Years");
+                           break;
+            case "march" : 
+            case "mar" :   System.out.print("The No Of Days In This Month : 31 Days");
+                           break;
+            case "april" : 
+            case "apr" :   System.out.print("The No Of Days In This Month : 30 Days");
+                           break; 
+            case "may" :   System.out.print("The No Of Days In This Month : 31 Days");
+                           break;
+            case "june" : 
+            case "jun" :   System.out.print("The No Of Days In This Month : 30 Days");
+                           break;
+            case "july" : 
+            case "jul" :   System.out.print("The No Of Days In This Month : 31 Days");
+                           break;
+            case "august" : 
+            case "aug" :   System.out.print("The No Of Days In This Month : 31 Days");
+                           break;
+            case "september" : 
+            case "sep" :
+            case "sept" :   System.out.print("The No Of Days In This Month : 30 Days");
+                            break;
+            case "october" :
+            case "oct" :   System.out.print("The No Of Days In This Month : 31 Days");
+                           break;
+            case "november" :
+            case "nov" :   System.out.print("The No Of Days In This Month : 30 Days");
+                           break;
+            case "december" :
+            case "dec" :   System.out.print("The No Of Days In This Month : 31 Days");
+                           break;
+            default :      System.out.print("Please Enter A Valid Month.....");
+        }
+    }
+    
+}
